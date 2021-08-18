6931e46d5b6ca529815252274baf8cd14e755f41
Joshua Williams <jmatthewwilliams89@gmail.com>
2021-08-18 16:25:11 -0400

    Agent pull apart (#55)
    * First pass at pulling agent from collector
    
    * Updated with go mod tidy
    
    * Updated CI/CD and version info

--- receiver/logsreceiver/register_linux.go
+++
@@ -1,6 +0,0 @@
-package logsreceiver
-
-import (
-	// Load linux only packages when importing input operators
-	_ "github.com/open-telemetry/opentelemetry-log-collection/operator/builtin/input/journald"
-)
