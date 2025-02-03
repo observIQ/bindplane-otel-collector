observIQ/bindplane-agent

Commit actual install_unix.sh script

scripts/install/install_unix.sh
@@ -917,7 +917,6 @@ create_supervisor_config() {
 }
 
 # This will display the results of an installation
-<<<<<<< HEAD
 display_results() {
   banner 'Information'
   increase_indent
@@ -927,36 +926,17 @@ display_results() {
   if [ "$SVC_PRE" = "systemctl" ]; then
     info "Supervisor Start Command:      $(fg_cyan "sudo systemctl start observiq-otel-collector")$(reset)"
     info "Supervisor Stop Command:       $(fg_cyan "sudo systemctl stop observiq-otel-collector")$(reset)"
-    info "Status Command:     $(fg_cyan "sudo systemctl status observiq-otel-collector")$(reset)"
-  else
+    info "Supervisor Status Command:     $(fg_cyan "sudo systemctl status observiq-otel-collector")$(reset)"
+  elif [ "$SVC_PRE" = "service" ]; then
     info "Supervisor Start Command:      $(fg_cyan "sudo service observiq-otel-collector start")$(reset)"
     info "Supervisor Stop Command:       $(fg_cyan "sudo service observiq-otel-collector stop")$(reset)"
-    info "Status Command:     $(fg_cyan "sudo service observiq-otel-collector status")$(reset)"
+    info "Supervisor Status Command:     $(fg_cyan "sudo service observiq-otel-collector status")$(reset)"
+  elif [ "$SVC_PRE" = "mkssys" ]; then
+    info "Supervisor Start Command:      $(fg_cyan "sudo startsrc -s observiq-otel-collector -a start -e "$(cat /opt/observiq-otel-collector/observiq-otel-collector.env)"")$(reset)"
+    info "Supervisor Stop Command:       $(fg_cyan "sudo stopsrc -s observiq-otel-collector")$(reset)"
+    info "Supervisor Status Command:     $(fg_cyan "sudo lssrc -s observiq-otel-collector")$(reset)"
   fi
   decrease_indent
-=======
-display_results()
-{
-    banner 'Information'
-    increase_indent
-    info "Agent Home:         $(fg_cyan "/opt/observiq-otel-collector")$(reset)"
-    info "Agent Config:       $(fg_cyan "/opt/observiq-otel-collector/config.yaml")$(reset)"
-    if [ "$SVC_PRE" = "systemctl" ]; then
-      info "Start Command:      $(fg_cyan "sudo systemctl start observiq-otel-collector")$(reset)"
-      info "Stop Command:       $(fg_cyan "sudo systemctl stop observiq-otel-collector")$(reset)"
-      info "Status Command:     $(fg_cyan "sudo systemctl status observiq-otel-collector")$(reset)"
-    elif [ "$SVC_PRE" = "service" ]; then
-      info "Start Command:      $(fg_cyan "sudo service observiq-otel-collector start")$(reset)"
-      info "Stop Command:       $(fg_cyan "sudo service observiq-otel-collector stop")$(reset)"
-      info "Status Command:     $(fg_cyan "sudo service observiq-otel-collector status")$(reset)"
-    elif [ "$SVC_PRE" = "mkssys" ]; then
-      info "Start Command:      $(fg_cyan "sudo startsrc -s observiq-otel-collector -a start -e \"\$(cat /opt/observiq-otel-collector/observiq-otel-collector.env)\"")$(reset)"
-      info "Stop Command:       $(fg_cyan "sudo stopsrc -s observiq-otel-collector")$(reset)"
-      info "Status Command:     $(fg_cyan "sudo lssrc -s observiq-otel-collector")$(reset)"
-    fi
-    info "Logs Command:       $(fg_cyan "sudo tail -F /opt/observiq-otel-collector/log/collector.log")$(reset)"
-    decrease_indent
->>>>>>> 40501763 (Add AIX messages, and status for all, to the agent information output of install script)
 
   banner 'Support'
   increase_indent

observIQ/bindplane-otel-collector

scripts/install/install_unix.sh
@@ -922,7 +922,6 @@ create_supervisor_config() {
 }
 
 # This will display the results of an installation
-<<<<<<< HEAD
 display_results() {
   banner 'Information'
   increase_indent
@@ -932,36 +931,17 @@ display_results() {
   if [ "$SVC_PRE" = "systemctl" ]; then
     info "Supervisor Start Command:      $(fg_cyan "sudo systemctl start bindplane-otel-collector")$(reset)"
     info "Supervisor Stop Command:       $(fg_cyan "sudo systemctl stop bindplane-otel-collector")$(reset)"
-    info "Status Command:     $(fg_cyan "sudo systemctl status bindplane-otel-collector")$(reset)"
-  else
+    info "Supervisor Status Command:     $(fg_cyan "sudo systemctl status bindplane-otel-collector")$(reset)"
+  elif [ "$SVC_PRE" = "service" ]; then
     info "Supervisor Start Command:      $(fg_cyan "sudo service bindplane-otel-collector start")$(reset)"
     info "Supervisor Stop Command:       $(fg_cyan "sudo service bindplane-otel-collector stop")$(reset)"
-    info "Status Command:     $(fg_cyan "sudo service bindplane-otel-collector status")$(reset)"
+    info "Supervisor Status Command:     $(fg_cyan "sudo service bindplane-otel-collector status")$(reset)"
+  elif [ "$SVC_PRE" = "mkssys" ]; then
+    info "Supervisor Start Command:      $(fg_cyan "sudo startsrc -s bindplane-otel-collector -a start -e "$(cat /opt/bindplane-otel-collector/bindplane-otel-collector.env)"")$(reset)"
+    info "Supervisor Stop Command:       $(fg_cyan "sudo stopsrc -s bindplane-otel-collector")$(reset)"
+    info "Supervisor Status Command:     $(fg_cyan "sudo lssrc -s bindplane-otel-collector")$(reset)"
   fi
   decrease_indent
-=======
-display_results()
-{
-    banner 'Information'
-    increase_indent
-    info "Agent Home:         $(fg_cyan "/opt/observiq-otel-collector")$(reset)"
-    info "Agent Config:       $(fg_cyan "/opt/observiq-otel-collector/config.yaml")$(reset)"
-    if [ "$SVC_PRE" = "systemctl" ]; then
-      info "Start Command:      $(fg_cyan "sudo systemctl start observiq-otel-collector")$(reset)"
-      info "Stop Command:       $(fg_cyan "sudo systemctl stop observiq-otel-collector")$(reset)"
-      info "Status Command:     $(fg_cyan "sudo systemctl status observiq-otel-collector")$(reset)"
-    elif [ "$SVC_PRE" = "service" ]; then
-      info "Start Command:      $(fg_cyan "sudo service observiq-otel-collector start")$(reset)"
-      info "Stop Command:       $(fg_cyan "sudo service observiq-otel-collector stop")$(reset)"
-      info "Status Command:     $(fg_cyan "sudo service observiq-otel-collector status")$(reset)"
-    elif [ "$SVC_PRE" = "mkssys" ]; then
-      info "Start Command:      $(fg_cyan "sudo startsrc -s observiq-otel-collector -a start -e \"\$(cat /opt/observiq-otel-collector/observiq-otel-collector.env)\"")$(reset)"
-      info "Stop Command:       $(fg_cyan "sudo stopsrc -s observiq-otel-collector")$(reset)"
-      info "Status Command:     $(fg_cyan "sudo lssrc -s observiq-otel-collector")$(reset)"
-    fi
-    info "Logs Command:       $(fg_cyan "sudo tail -F /opt/observiq-otel-collector/log/collector.log")$(reset)"
-    decrease_indent
->>>>>>> 40501763 (Add AIX messages, and status for all, to the agent information output of install script)
 
   banner 'Support'
   increase_indent