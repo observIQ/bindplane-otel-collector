208db0b82c5417c1c8e6b6fd828005caa5ff08cb
Daniel Kuiper <44123852+kuiperda@users.noreply.github.com>
2025-02-25 13:39:32 -0500

    update config

--- exporter/googlecloudstorageexporter/config.go
+++ exporter/googlecloudstorageexporter/config.go
@@ -2,16 +2,27 @@ package googlecloudstorageexporter // import "github.com/observiq/bindplane-otel
 
 import (
 	"errors"
+	"fmt"
 
 	"go.opentelemetry.io/collector/config/configretry"
 	"go.opentelemetry.io/collector/exporter/exporterhelper"
 )
 
-type Config struct {
-	exporterhelper.TimeoutConfig `mapstructure:",squash"` // squash ensures fields are correctly decoded in embedded struct.
-	exporterhelper.QueueConfig   `mapstructure:"sending_queue"`
-	configretry.BackOffConfig `mapstructure:"retry_on_failure"`
+type partitionType string
 
+const (
+	minutePartition partitionType = "minute"
+	hourPartition   partitionType = "hour"
+)
+
+type compressionType string
+
+const (
+	noCompression   compressionType = "none"
+	gzipCompression compressionType = "gzip"
+)
+
+type Config struct {
 	ProjectID  string `mapstructure:"project_id"`
 	BucketName string `mapstructure:"bucket_name"`
 	Location   string `mapstructure:"location"`
@@ -19,6 +30,13 @@ type Config struct {
 	FolderName string `mapstructure:"folder_name"`
 	ObjectPrefix string `mapstructure:"object_prefix"`
 	// CredentialsFile string `mapstructure:"credentials_file"`
+
+	Partition partitionType `mapstructure:"partition"`
+	Compression compressionType `mapstructure:"compression"`
+
+	exporterhelper.TimeoutConfig `mapstructure:",squash"` // squash ensures fields are correctly decoded in embedded struct.
+	exporterhelper.QueueConfig   `mapstructure:"sending_queue"`
+	configretry.BackOffConfig `mapstructure:"retry_on_failure"`
 }
 
 func (c *Config) Validate() error {
@@ -28,15 +46,24 @@ func (c *Config) Validate() error {
 	if c.ProjectID == "" {
 		return errors.New("project_id is required")
 	}
+	if c.Location == "" {
+		return errors.New("location is required")
+	}
+
+	switch c.Partition {
+	case minutePartition, hourPartition:
+	// do nothing
+	default:
+		return fmt.Errorf("unsupported partition type '%s'", c.Partition)
+	}
+
+	switch c.Compression {
+	case noCompression, gzipCompression:
+		return nil
+	default:
+		return fmt.Errorf("unsupported compression type: %s", c.Compression)
+	}
+	
 	return nil
 
-	// if c.FolderName == "" {
-	// 	return errors.New("folder_name is required")
-	// }
-
-	// if c.BlobPrefix == "" {
-	// 	return errors.New("blob_prefix is required")
-	// }
-	
-	
 }
