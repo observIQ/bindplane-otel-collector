f33e1e043b24242ac2bf203077bddc09e511fd9b
Daniel Kuiper <44123852+kuiperda@users.noreply.github.com>
2025-02-25 13:10:12 -0500

    more updates

--- exporter/googlecloudstorageexporter/exporter.go
+++ exporter/googlecloudstorageexporter/exporter.go
@@ -3,9 +3,6 @@ package googlecloudstorageexporter // import "github.com/observiq/bindplane-otel
 import (
 	"context"
 	"fmt"
-	"math/rand"
-	"strings"
-	"time"
 
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/exporter"
@@ -22,11 +19,12 @@ type googleCloudStorageExporter struct {
 	marshaler  marshaler
 }
 
-func newExporter(cfg *Config, params exporter.CreateSettings) (*googleCloudStorageExporter, error) {
-	storageClient, err := newGoogleCloudStorageClient(cfg.ConnectionString)
+func newExporter(cfg *Config, params exporter.Settings) (*googleCloudStorageExporter, error) {
+	storageClient, err := newGoogleCloudStorageClient()
 	if err != nil {
 		return nil, fmt.Errorf("failed to create storage client: %w", err)
 	}
+	// should close the client?
 
 	return &googleCloudStorageExporter{
 		cfg: cfg,
@@ -36,93 +34,93 @@ func newExporter(cfg *Config, params exporter.CreateSettings) (*googleCloudStora
 	}, nil
 }
 
-func (a *googleCloudStorageExporter) Capabilities() consumer.Capabilities {
+func (g *googleCloudStorageExporter) Capabilities() consumer.Capabilities {
 	return consumer.Capabilities{MutatesData: false}
 }
 
-func (a *googleCloudStorageExporter) metricsDataPusher(_ context.Context, md pmetric.Metrics) error {
-	buf, err := a.marshaler.MarshalMetrics(md)
+func (g *googleCloudStorageExporter) metricsDataPusher(ctx context.Context, md pmetric.Metrics) error {
+	buf, err := g.marshaler.MarshalMetrics(md)
 	if err != nil {
 		return fmt.Errorf("failed to marshal metrics: %w", err)
 	}
 
-	blobName := a.getBlobName("metrics")
+	blobName := g.getBlobName("metrics")
 
-	return a.uploadBuffer(ctx, blobName, buf)
+	return g.uploadBuffer(ctx, blobName, buf)
 }
 
-func (a *googleCloudStorageExporter) logsDataPusher(_ context.Context, ld plog.Logs) error {
-	buf, err := a.marshaler.MarshalLogs(ld)
+func (g *googleCloudStorageExporter) logsDataPusher(ctx context.Context, ld plog.Logs) error {
+	buf, err := g.marshaler.MarshalLogs(ld)
 	if err != nil {
 		return fmt.Errorf("failed to marshal logs: %w", err)
 	}
 
-	blobName := a.getBlobName("logs")
+	blobName := g.getBlobName("logs")
 
-	return a.uploadBuffer(ctx, blobName, buf)
+	return g.uploadBuffer(ctx, blobName, buf)
 }
 
-func (a *googleCloudStorageExporter) traceDataPusher(_ context.Context, td ptrace.Traces) error {
-	buf, err := a.marshaler.MarshalTraces(td)
+func (g *googleCloudStorageExporter) traceDataPusher(ctx context.Context, td ptrace.Traces) error {
+	buf, err := g.marshaler.MarshalTraces(td)
 	if err != nil {
 		return fmt.Errorf("failed to marshal traces: %w", err)
 	}
 
-	blobName := a.getBlobName("traces")
+	blobName := g.getBlobName("traces")
 
-	return a.uploadBuffer(ctx, blobName, buf)
+	return g.uploadBuffer(ctx, blobName, buf)
 }
 
-func (a *googleCloudStorageExporter) getBlobName(telemetryType string) string {
-	now := time.Now()
-	year, month, day := now.Date()
-	hour, minute, _ := now.Clock()
+// func (g *googleCloudStorageExporter) getBlobName(telemetryType string) string {
+// 	now := time.Now()
+// 	year, month, day := now.Date()
+// 	hour, minute, _ := now.Clock()
 
-	blobNameBuilder := strings.Builder{}
+// 	blobNameBuilder := strings.Builder{}
 
-	if a.cfg.FolderName != "" {
-		blobNameBuilder.WriteString(a.cfg.FolderName)
-		blobNameBuilder.WriteString("/")
-	}
+// 	if g.cfg.FolderName != "" {
+// 		blobNameBuilder.WriteString(g.cfg.FolderName)
+// 		blobNameBuilder.WriteString("/")
+// 	}
 
-	blobNameBuilder.WriteString(fmt.Sprintf("%s-%d-%d-%d-%d-%d", telemetryType, year, month, day, hour, minute))
+// 	blobNameBuilder.WriteString(fmt.Sprintf("%s-%d-%d-%d-%d-%d", telemetryType, year, month, day, hour, minute))
 
-	if a.cfg.BlobPrefix != "" {
-		blobNameBuilder.WriteString(fmt.Sprintf("-%s", a.cfg.BlobPrefix))
-	}
+// 	if g.cfg.ObjectPrefix != "" {
+// 		blobNameBuilder.WriteString(fmt.Sprintf("-%s", g.cfg.ObjectPrefix))
+// 	}
 
-	randomID := randomInRange(100000000, 999999999)
+// 	randomID := randomInRange(100000000, 999999999)
 
-	blobNameBuilder.WriteString(fmt.Sprintf("-%d", randomID))
+// 	blobNameBuilder.WriteString(fmt.Sprintf("-%d", randomID))
 
-	return blobNameBuilder.String()
-}
+// 	return blobNameBuilder.String()
+// }
 
-func (a *googleCloudStorageExporter) uploadBuffer(ctx context.Context, blobName string, buf []byte) error {
-	bucket := a.storageClient.Bucket(a.cfg.BucketName)
-	if err != nil {
-		return fmt.Errorf("failed to create bucket: %w", err)
-	}
+// func (g *googleCloudStorageExporter) uploadBuffer(ctx context.Context, blobName string, buf []byte) error {
+// 	bucket, err := g.storageClient.GetBucket(ctx, g.cfg.BucketName)
+// 	if err != nil {
+// 		return fmt.Errorf("failed to create bucket: %w", err)
+// 	}
 
-	object := bucket.Object(blobName)
-	if err != nil {
-		return fmt.Errorf("failed to create object: %w", err)
-	}
+// 	object := bucket.Object(blobName)
+// 	if err != nil {
+// 		return fmt.Errorf("failed to create object: %w", err)
+// 	}
 
-	writer := object.NewWriter(ctx)
-	writer.Write(buf)
-	if err != nil {
-		return fmt.Errorf("failed to write to object: %w", err)
-	}
+// 	writer := object.NewWriter(ctx)
+// 	writer.Write(buf)
+// 	if err != nil {
+// 		return fmt.Errorf("failed to write to object: %w", err)
+// 	}
 
-	err = writer.Close()
-	if err != nil {
-		return fmt.Errorf("failed to close writer: %w", err)
-	}
+// 	err = writer.Close()
+// 	if err != nil {
+// 		return fmt.Errorf("failed to close writer: %w", err)
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func randomInRange(low, hi int) int {
-	return low + rand.Intn(hi-low)
-}
+// func randomInRange(low, hi int) int {
+// 	return low + rand.Intn(hi-low)
+// }
