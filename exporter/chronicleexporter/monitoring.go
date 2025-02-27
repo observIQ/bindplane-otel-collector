90f7b92eced52d9535bd9692c25bc50c8f224806
Joe Sirianni <joe.sirianni@observiq.com>
2025-02-27 15:46:35 -0500

    polish metrics

--- exporter/chronicleexporter/monitoring.go
+++ exporter/chronicleexporter/monitoring.go
@@ -17,20 +17,14 @@ func setupMetrics() error {
 	var err error
 
 	batchLogCount, err = mp.Int64Histogram(
-		"otelcol_exporter_batch_log_count",
+		"otelcol_exporter_batch_size",
 		metric.WithDescription("The number of logs in a batch"),
 		metric.WithExplicitBucketBoundaries(
 			1,
-			10,
 			100,
-			200,
-			300,
-			400,
+			250,
 			500,
-			600,
-			700,
-			800,
-			900,
+			750,
 			1000,
 		),
 	)
@@ -39,74 +33,49 @@ func setupMetrics() error {
 	}
 
 	payloadSizeBytes, err = mp.Int64Histogram(
-		"otelcol_exporter_payload_size_bytes",
+		"otelcol_exporter_payload_size",
 		metric.WithDescription("The size of the payload in bytes"),
-		// TODO(jsirianni): What is the max payload size secops accepts?
 		metric.WithExplicitBucketBoundaries(
-			250, // typical single log
+			100,
 			500,
-			1000, // typical large log
-			5000,
-			10000,
-			50000,
-			100000, // 0.10mb
-			150000,
-			200000,
-			250000, // typical single log in 1,000 batch
-			300000,
-			350000,
-			400000,
-			450000,
-			500000, // 0.50mb
-			550000,
-			600000,
-			650000,
-			700000,
-			750000,
-			800000,
-			850000,
-			900000,
-			950000,
+			1000,    // typical large log
+			100000,  // 0.10mb
+			500000,  // 0.50mb
 			1000000, // 1mb
 			2000000,
 			3000000,
 			4000000,
 			5000000, // 5mb
 		),
+		metric.WithUnit("B"),
 	)
 	if err != nil {
 		return err
 	}
 
 	requestLatencyMilliseconds, err = mp.Int64Histogram(
-		"otelcol_exporter_request_latency_milliseconds",
+		"otelcol_exporter_request_latency",
 		metric.WithDescription("The latency of the request in milliseconds"),
 		metric.WithExplicitBucketBoundaries(
 			100,
-			150,
 			200,
-			250,
 			300,
-			350,
 			400,
-			450,
 			500,
-			550,
 			600,
-			650,
 			700,
-			750,
 			800,
-			850,
 			900,
-			950,
-			1000,  // 1 second
+			1000, // 1 second
+			2000,
+			5000,
 			10000, // 10 seconds
-			15000, // 15 seconds
-			20000, // 20 seconds
-			30000, // 30 seconds
+			15000,
+			20000,
+			30000,
 			60000, // 1 minute
 		),
+		metric.WithUnit("ms"),
 	)
 
 	return nil
