7aecfb64f9840f552b589312db54380de1d654f5
Daniel Kuiper <44123852+kuiperda@users.noreply.github.com>
2025-03-03 15:03:34 -0500

    feat: Add Google Cloud Storage Exporter (#2194)
    * stub exporter
    
    * start config.go
    
    * stub (broken) factory.go and run mdatagen
    
    * stub exporter
    
    * fill out (broken) exporter
    
    * add exporter to factory
    
    * add broken storage client
    
    * add create bucket func
    
    * update client
    
    * update storage client
    
    * update factory.go
    
    * update marshaler
    
    * more updates
    
    * update config
    
    * update nil return
    
    * update marshaler
    
    * update config in factory.go
    
    * update exporter
    
    * add creds
    
    * update creds
    
    * finish add creds
    
    * go mod tidy
    
    * mdatagen and go mod tidy
    
    * edit component test to be correct despite warnings not to edit
    
    * actually create the bucket before writing to it
    
    * create bucket unless conflict
    
    * assume bucket exists
    
    * list buckets for logging
    
    * check if bucket exists and create if not
    
    * update folder structure
    
    * add license to more files
    
    * config is good
    
    * fixes from config renames
    
    * add config_test
    
    * finish exporter.go
    
    * generate storage client mocks
    
    * add exporter tests
    
    * update factory and add factory test
    
    * add marshaler test
    
    * update metadata.yaml
    
    * pass in cfg instead of individual fields to storage client
    
    * mdatagen and update exporter test with new signatures
    
    * update storage client to be simpler
    
    * update storage client to be even simpler
    
    * improve storage client error messages
    
    * mdatagen update
    
    * simplify write err handling
    
    * fill out readme
    
    * add note about permissions
    
    * make fmt
    
    * lint
    
    * gosec and comments
    
    * try fixing component lifecycle tests
    
    * gosec
    
    * skip shutdown
    
    * mdatagen, go mod tidy, make fmt
    
    * go mod tidy in correct dir
    
    * remove redundant err msg language
    
    * wrap errors
    
    * set project id from creds if not provided
    
    * make fmt and go mod tidy
    
    * set project id from default creds
    
    * document exporterhelper config options

---
+++ exporter/googlecloudstorageexporter/config.go
@@ -0,0 +1,109 @@
+// Copyright observIQ, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package googlecloudstorageexporter // import "github.com/observiq/bindplane-otel-collector/exporter/googlecloudstorageexporter"
+
+import (
+	"errors"
+	"fmt"
+
+	"go.opentelemetry.io/collector/config/configretry"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
+)
+
+// partitionType is the type of partition to store objects under
+type partitionType string
+
+const (
+	minutePartition partitionType = "minute"
+	hourPartition   partitionType = "hour"
+)
+
+// compressionType is the type of compression to apply to objects
+type compressionType string
+
+const (
+	noCompression   compressionType = "none"
+	gzipCompression compressionType = "gzip"
+)
+
+// Config is the configuration for the googlecloudstorage exporter
+type Config struct {
+	// ProjectID is the ID of the Google Cloud project the bucket belongs to.
+	ProjectID string `mapstructure:"project_id"`
+
+	// BucketName is the name of the bucket to store objects in.
+	BucketName string `mapstructure:"bucket_name"`
+
+	// BucketLocation is the location of the bucket.
+	BucketLocation string `mapstructure:"bucket_location"`
+
+	// BucketStorageClass is the storage class of the bucket.
+	BucketStorageClass string `mapstructure:"bucket_storage_class"`
+
+	// FolderName is the name of the folder to store objects under.
+	FolderName string `mapstructure:"folder_name"`
+
+	// ObjectPrefix is the prefix to add to the object name.
+	ObjectPrefix string `mapstructure:"object_prefix"`
+
+	// Credentials and CredentialsFile are mutually exclusive and provide authentication to Google Cloud Storage.
+	Credentials     string `mapstructure:"credentials"`
+	CredentialsFile string `mapstructure:"credentials_file"`
+
+	// Partition is the time granularity of the object.
+	// Valid values are "hour" or "minute". Default: minute
+	Partition partitionType `mapstructure:"partition"`
+
+	// Compression is the type of compression to use.
+	// Valid values are "none" or "gzip". Default: none
+	Compression compressionType `mapstructure:"compression"`
+
+	// TimeoutConfig configures timeout settings for exporter operations.
+	exporterhelper.TimeoutConfig `mapstructure:",squash"` // squash ensures fields are correctly decoded in embedded struct.
+
+	// QueueConfig defines the queuing behavior for the exporter.
+	exporterhelper.QueueConfig `mapstructure:"sending_queue"`
+
+	// BackOffConfig defines the retry behavior for failed operations.
+	configretry.BackOffConfig `mapstructure:"retry_on_failure"`
+}
+
+// Validate validates the config.
+func (c *Config) Validate() error {
+	if c.BucketName == "" {
+		return errors.New("bucket_name is required")
+	}
+
+	// Validate credentials - both can be empty (for default credentials) but both cannot be set
+	if c.Credentials != "" && c.CredentialsFile != "" {
+		return errors.New("cannot specify both credentials and credentials_file")
+	}
+
+	switch c.Partition {
+	case minutePartition, hourPartition:
+	// do nothing
+	default:
+		return fmt.Errorf("unsupported partition type '%s'", c.Partition)
+	}
+
+	switch c.Compression {
+	case noCompression, gzipCompression:
+	// do nothing
+	default:
+		return fmt.Errorf("unsupported compression type: %s", c.Compression)
+	}
+
+	return nil
+}
---
+++ exporter/googlecloudstorageexporter/exporter.go
@@ -0,0 +1,136 @@
+// Copyright observIQ, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package googlecloudstorageexporter // import "github.com/observiq/bindplane-otel-collector/exporter/googlecloudstorageexporter"
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/exporter"
+	"go.opentelemetry.io/collector/pdata/plog"
+	"go.opentelemetry.io/collector/pdata/pmetric"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+	"go.uber.org/zap"
+)
+
+// googleCloudStorageExporter exports OTLP data as Google Cloud Storage objects
+type googleCloudStorageExporter struct {
+	cfg           *Config
+	storageClient storageClient
+	logger        *zap.Logger
+	marshaler     marshaler
+}
+
+// newExporter creates a new Google Cloud Storage exporter
+func newExporter(cfg *Config, params exporter.Settings) (*googleCloudStorageExporter, error) {
+	storageClient, err := newGoogleCloudStorageClient(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("create storage client: %w", err)
+	}
+
+	return &googleCloudStorageExporter{
+		cfg:           cfg,
+		storageClient: storageClient,
+		logger:        params.Logger,
+		marshaler:     newMarshaler(cfg.Compression),
+	}, nil
+}
+
+// Capabilities lists the exporter's capabilities
+func (g *googleCloudStorageExporter) Capabilities() consumer.Capabilities {
+	return consumer.Capabilities{MutatesData: false}
+}
+
+// metricsDataPusher pushes metrics data to Google Cloud Storage
+func (g *googleCloudStorageExporter) metricsDataPusher(ctx context.Context, md pmetric.Metrics) error {
+	buf, err := g.marshaler.MarshalMetrics(md)
+	if err != nil {
+		return fmt.Errorf("marshal metrics: %w", err)
+	}
+
+	objectName := g.getObjectName("metrics")
+
+	return g.storageClient.UploadObject(ctx, objectName, buf)
+}
+
+// logsDataPusher pushes logs data to Google Cloud Storage
+func (g *googleCloudStorageExporter) logsDataPusher(ctx context.Context, ld plog.Logs) error {
+	buf, err := g.marshaler.MarshalLogs(ld)
+	if err != nil {
+		return fmt.Errorf("marshal logs: %w", err)
+	}
+
+	objectName := g.getObjectName("logs")
+
+	return g.storageClient.UploadObject(ctx, objectName, buf)
+}
+
+// tracesDataPusher pushes trace data to Google Cloud Storage
+func (g *googleCloudStorageExporter) tracesDataPusher(ctx context.Context, td ptrace.Traces) error {
+	buf, err := g.marshaler.MarshalTraces(td)
+	if err != nil {
+		return fmt.Errorf("marshal traces: %w", err)
+	}
+
+	objectName := g.getObjectName("traces")
+
+	return g.storageClient.UploadObject(ctx, objectName, buf)
+}
+
+// getObjectName formats the object name based on the configuration and current time stamp
+func (g *googleCloudStorageExporter) getObjectName(telemetryType string) string {
+	now := time.Now().UTC()
+	year, month, day := now.Date()
+	hour, minute, _ := now.Clock()
+
+	objectNameBuilder := strings.Builder{}
+
+	// Add folder name if specified
+	if g.cfg.FolderName != "" {
+		objectNameBuilder.WriteString(fmt.Sprintf("%s/", g.cfg.FolderName))
+	}
+
+	// Add hierarchical time-based folders
+	objectNameBuilder.WriteString(fmt.Sprintf("year=%d/month=%02d/day=%02d/hour=%02d", year, month, day, hour))
+
+	// Add minute folder if using minute partitioning
+	if g.cfg.Partition == minutePartition {
+		objectNameBuilder.WriteString(fmt.Sprintf("/minute=%02d", minute))
+	}
+
+	objectNameBuilder.WriteString("/")
+
+	// Add object prefix if specified
+	if g.cfg.ObjectPrefix != "" {
+		objectNameBuilder.WriteString(g.cfg.ObjectPrefix)
+	}
+
+	// Generate a random ID for the name
+	randomID := randomInRange(100000000, 999999999)
+
+	// Write base file name with telemetry type and random ID
+	objectNameBuilder.WriteString(fmt.Sprintf("%s_%d.%s", telemetryType, randomID, g.marshaler.Format()))
+
+	return objectNameBuilder.String()
+}
+
+// #nosec G404 -- randomly generated number is not used for security purposes. It's ok if it's weak
+func randomInRange(low, hi int) int {
+	return low + rand.Intn(hi-low)
+}
