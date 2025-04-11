44693a7d31b3fff2da2b93f095bbe8837e425c2c
Dakota Paasman <122491662+dpaasman00@users.noreply.github.com>
2025-04-11 13:21:24 -0400

    update comment for sudo too

--- scripts/generic-install/install_unix.sh
+++ scripts/generic-install/install_unix.sh
@@ -360,7 +360,7 @@ parse_args() {
 
 check_root() {
     if [ "$(id -u)" != "0" ]; then
-        echo "This script must be run as root"
+        echo "This script must be run as root or using sudo"
         exit 1
     fi
 }
