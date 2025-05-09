observIQ/bindplane-agent

Add Makefile changes

Makefile
@@ -44,10 +44,16 @@ updater:
 build-binaries: agent updater
 
 .PHONY: build-all
-build-all: build-linux build-darwin build-windows
+build-all: build-linux build-unix build-windows
 
 .PHONY: build-linux
 build-linux: build-linux-amd64 build-linux-arm64 build-linux-arm build-linux-ppc64 build-linux-ppc64le
+
+.PHONY: build-unix
+build-unix: build-darwin build-aix
+
+.PHONY: build-aix
+build-aix: build-aix-ppc64
 
 .PHONY: build-darwin
 build-darwin: build-darwin-amd64 build-darwin-arm64
@@ -74,6 +80,10 @@ build-linux-arm64:
 .PHONY: build-linux-arm
 build-linux-arm:
 	GOOS=linux GOARCH=arm $(MAKE) build-binaries -j2
+
+.PHONY: build-aix-ppc64
+build-aix-ppc64:
+	GOOS=aix GOARCH=ppc64 $(MAKE) build-binaries -j2
 
 .PHONY: build-darwin-amd64
 build-darwin-amd64:
@@ -217,6 +227,7 @@ release-prep:
 	@jq ".files[] | select(.service != null)" windows/wix.json >> release_deps/windows_service.json
 	@cp service/observiq-otel-collector.service release_deps/observiq-otel-collector.service
 	@cp service/observiq-otel-collector release_deps/observiq-otel-collector
+	@cp service/observiq-otel-collector.aix.env release_deps/observiq-otel-collector.aix.env
 	@cp -r ./service/sysconfig release_deps/
 
 # Build and sign, skip release and ignore dirty git tree

observIQ/bindplane-otel-collector

Makefile
@@ -60,10 +60,16 @@ reset: kill
 	rm -rf agent.log effective.yaml local/storage/* builder/
 
 .PHONY: build-all
-build-all: build-linux build-darwin build-windows
+build-all: build-linux build-unix build-windows
 
 .PHONY: build-linux
 build-linux: build-linux-amd64 build-linux-arm64 build-linux-arm build-linux-ppc64 build-linux-ppc64le
+
+.PHONY: build-unix
+build-unix: build-darwin build-aix
+
+.PHONY: build-aix
+build-aix: build-aix-ppc64
 
 .PHONY: build-darwin
 build-darwin: build-darwin-amd64 build-darwin-arm64
@@ -90,6 +96,10 @@ build-linux-arm64:
 .PHONY: build-linux-arm
 build-linux-arm:
 	GOOS=linux GOARCH=arm $(MAKE) collector
+
+.PHONY: build-aix-ppc64
+build-aix-ppc64:
+	GOOS=aix GOARCH=ppc64 $(MAKE) build-binaries -j2
 
 .PHONY: build-darwin-amd64
 build-darwin-amd64:
@@ -234,6 +244,7 @@ release-prep:
 	@jq ".files[] | select(.service != null)" windows/wix.json >> release_deps/windows_service.json
 	@cp service/bindplane-otel-collector.service release_deps/bindplane-otel-collector.service
 	@cp service/bindplane-otel-collector release_deps/bindplane-otel-collector
+	@cp service/bindplane-otel-collector.aix.env release_deps/bindplane-otel-collector.aix.env
 	@cp -r ./service/sysconfig release_deps/
 
 # Build and sign, skip release and ignore dirty git tree