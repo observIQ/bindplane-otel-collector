20caa36f6a8717a9b8f0090904643272c095226a
Joe Sirianni <joe.sirianni@observiq.com>
2025-06-06 14:14:20 -0400

    feat(linuxpackage): Support skipping Linux user creation

--- scripts/package/preinstall.sh
+++ scripts/package/preinstall.sh
@@ -16,18 +16,38 @@
 
 set -e
 
+# Read's optional package overrides. Users should deploy the override
+# file before installing BDOT for the first time. The override should
+# not be modified unless uninstalling and re-installing.
+[ -f /etc/default/observiq-otel-collector ] && . /etc/default/observiq-otel-collector
+[ -f /etc/sysconfig/observiq-otel-collector ] && . /etc/sysconfig/observiq-otel-collector
+
+: "${BDOT_SKIP_RUNTIME_USER_CREATION:=false}"
+
 username="observiq-otel-collector"
 
-if getent group "$username" > /dev/null 2>&1; then
-    echo "Group ${username} already exists."
-else
-    groupadd "$username"
-fi
+install() {
+    if [ "$BDOT_SKIP_RUNTIME_USER_CREATION" = "true" ]; then
+        echo "BDOT_SKIP_RUNTIME_USER_CREATION is set to true, skipping user and group creation"
+    else
+        echo "Creating ${username} user and group"
+        install_user
+    fi
+}
 
-if id "$username" > /dev/null 2>&1; then
-    echo "User ${username} already exists"
-    exit 0
-else
-    useradd --shell /sbin/nologin --system "$username" -g "$username"
-fi
+install_user() {
+    if getent group "$username" > /dev/null 2>&1; then
+        echo "Group ${username} already exists."
+    else
+        groupadd "$username"
+    fi
 
+    if id "$username" > /dev/null 2>&1; then
+        echo "User ${username} already exists"
+        exit 0
+    else
+        useradd --shell /sbin/nologin --system "$username" -g "$username"
+    fi
+}
+
+install
