7dab55cef6e8ed25520a83ff0c6b4b278f48665a
Brian Gardner <brian.gardner@bindplane.com>
2025-07-10 13:10:06 -0400

    fix: error message

--- receiver/awss3eventreceiver/receiver.go
+++ receiver/awss3eventreceiver/receiver.go
@@ -87,7 +87,7 @@ func newLogsReceiver(id component.ID, tel component.TelemetrySettings, cfg *Conf
 		objectKeyFilter, err = regexp.Compile(cfg.ObjectKeyFilter)
 		if err != nil {
 			// config validation should have already caught this
-			return nil, fmt.Errorf("failed to compile regex filter: %w", err)
+			return nil, fmt.Errorf("failed to compile object_key_filter regex: %w", err)
 		}
 	}
 
