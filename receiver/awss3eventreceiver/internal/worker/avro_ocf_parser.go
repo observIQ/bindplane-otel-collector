d152ed4be1e6592a28ba15bf1efd00a6a1407a4b
Dakota Paasman <122491662+dpaasman00@users.noreply.github.com>
2025-07-17 15:06:40 -0400

    license & lint fix

--- receiver/awss3eventreceiver/internal/worker/avro_ocf_parser.go
+++ receiver/awss3eventreceiver/internal/worker/avro_ocf_parser.go
@@ -1,3 +1,17 @@
+// Copyright observIQ, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package worker
 
 import (
@@ -38,6 +52,7 @@ func NewAvroOcfParser(reader BufferedReader, logger *zap.Logger) LogParser {
 	}
 }
 
+// StartsWithAvroOcfMagic checks if the reader starts with the Avro OCF magic string.
 func StartsWithAvroOcfMagic(reader BufferedReader) (bool, error) {
 	bytes, err := reader.Peek(len(avroOcfMagicBytes))
 	if err != nil {
