53217eef96f0f2ef269370c64a888a19e25b8b25
Caleb-Hurshman <caleb.hurshman@bindplane.com>
2025-08-28 11:02:06 -0400

    basic tracev3 file receiver

---
+++ receiver/macosunifiedloggingreceiver/doc.go
@@ -0,0 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Package macosunifiedlogreceiver provides a receiver for parsing macOS Unified Logging traceV3 files.
+// It uses the Stanza framework for file consumption and supports encoding extensions for decoding.
+package macosunifiedloggingreceiver // import "github.com/observiq/bindplane-otel-collector/receiver/macosunifiedloggingreceiver"
