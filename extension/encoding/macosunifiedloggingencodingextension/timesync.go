9bdbed28fcc080562491ff41377e085a4e3b51c1
Caleb Hurshman <caleb.hurshman@bindplane.com>
2025-09-22 11:05:54 -0400

    Refactor Timesync Logic (PIPE-487) (#2666)
    * redo + test ParseTimesyncBoot
    
    * redo + test ParseTimesyncRecord
    
    * convert remaining funcs
    
    * parsing tests

--- extension/encoding/macosunifiedloggingencodingextension/timesync.go
+++ extension/encoding/macosunifiedloggingencodingextension/timesync.go
@@ -18,10 +18,11 @@ import (
 	"encoding/binary"
 	"fmt"
 	"strings"
+
+	"github.com/observiq/bindplane-otel-collector/extension/encoding/macosunifiedloggingencodingextension/internal/utils"
 )
 
 // TimesyncBoot represents a timesync boot record containing time correlation data
-// Based on the Rust implementation from mandiant/macos-UnifiedLogs
 type TimesyncBoot struct {
 	Signature           uint16           // Boot record signature (0xbbb0)
 	HeaderSize          uint16           // Size of the header
@@ -36,7 +37,6 @@ type TimesyncBoot struct {
 }
 
 // TimesyncRecord represents a single timesync record correlating mach time to wall time
-// Based on the Rust implementation from mandiant/macos-UnifiedLogs
 type TimesyncRecord struct {
 	Signature       uint32 // Record signature (0x207354)
 	UnknownFlags    uint32 // Unknown flags
@@ -46,157 +46,122 @@ type TimesyncRecord struct {
 	DaylightSavings uint32 // Daylight savings flag
 }
 
+const expectedTimesyncSignature = 0x207354
+
 // ParseTimesyncData parses timesync files and returns a map of boot UUIDs to TimesyncBoot records
-// Based on the Rust implementation logic
 func ParseTimesyncData(data []byte) (map[string]*TimesyncBoot, error) {
 	timesyncData := make(map[string]*TimesyncBoot)
-	offset := 0
-	var currentBoot *TimesyncBoot
+	timesyncBoot := &TimesyncBoot{}
+	var err error
 
-	for offset < len(data) {
-		if offset+4 > len(data) {
-			break
-		}
-
-		// Check the signature to determine record type
-		signature := binary.LittleEndian.Uint32(data[offset:])
-
-		if signature == 0x207354 { // Timesync record signature
-			record, newOffset, err := parseTimesyncRecord(data, offset)
+	for len(data) > 0 {
+		_, signature, _ := utils.Take(data, 4)
+		if binary.LittleEndian.Uint32(signature) == expectedTimesyncSignature {
+			timesyncRecord, remainingData, err := ParseTimesyncRecord(data)
 			if err != nil {
-				// Skip this record and continue
-				offset += 4
-				continue
+				return timesyncData, err
 			}
-
-			if currentBoot != nil {
-				currentBoot.TimesyncRecords = append(currentBoot.TimesyncRecords, *record)
-			}
-			offset = newOffset
-
+			timesyncBoot.TimesyncRecords = append(timesyncBoot.TimesyncRecords, *timesyncRecord)
+			data = remainingData
 		} else {
-			// Try to parse as boot record
-			boot, newOffset, err := parseTimesyncBoot(data, offset)
-			if err != nil {
-				// Skip this data and continue
-				offset += 4
-				continue
-			}
-
-			// Save previous boot record if it exists
-			if currentBoot != nil {
-				if existing, exists := timesyncData[currentBoot.BootUUID]; exists {
-					// Merge records if boot UUID already exists
-					existing.TimesyncRecords = append(existing.TimesyncRecords, currentBoot.TimesyncRecords...)
+			if timesyncBoot.Signature != 0 {
+				if existing, exists := timesyncData[timesyncBoot.BootUUID]; exists {
+					existing.TimesyncRecords = append(existing.TimesyncRecords, timesyncBoot.TimesyncRecords...)
 				} else {
-					timesyncData[currentBoot.BootUUID] = currentBoot
+					timesyncData[timesyncBoot.BootUUID] = timesyncBoot
 				}
 			}
-
-			currentBoot = boot
-			offset = newOffset
+			timesyncBoot, data, err = ParseTimesyncBoot(data)
+			if err != nil {
+				return timesyncData, err
+			}
 		}
 	}
-
-	// Save the last boot record
-	if currentBoot != nil {
-		if existing, exists := timesyncData[currentBoot.BootUUID]; exists {
-			existing.TimesyncRecords = append(existing.TimesyncRecords, currentBoot.TimesyncRecords...)
-		} else {
-			timesyncData[currentBoot.BootUUID] = currentBoot
-		}
+	if existing, exists := timesyncData[timesyncBoot.BootUUID]; exists {
+		existing.TimesyncRecords = append(existing.TimesyncRecords, timesyncBoot.TimesyncRecords...)
+	} else {
+		timesyncData[timesyncBoot.BootUUID] = timesyncBoot
 	}
-
 	return timesyncData, nil
 }
 
-// parseTimesyncBoot parses a timesync boot record
-func parseTimesyncBoot(data []byte, offset int) (*TimesyncBoot, int, error) {
-	if offset+48 > len(data) { // Minimum size for boot record
-		return nil, offset, fmt.Errorf("insufficient data for boot record")
-	}
-
-	signature := binary.LittleEndian.Uint16(data[offset:])
-	if signature != 0xbbb0 {
-		return nil, offset, fmt.Errorf("invalid boot signature: expected 0xbbb0, got 0x%x", signature)
-	}
-
+// ParseTimesyncBoot parses a timesync boot record
+func ParseTimesyncBoot(data []byte) (*TimesyncBoot, []byte, error) {
 	boot := &TimesyncBoot{}
-	boot.Signature = signature
-	offset += 2
-
-	boot.HeaderSize = binary.LittleEndian.Uint16(data[offset:])
-	offset += 2
-	boot.Unknown = binary.LittleEndian.Uint32(data[offset:])
-	offset += 4
-
-	// Boot UUID (16 bytes, big endian)
-	if offset+16 > len(data) {
-		return nil, offset, fmt.Errorf("insufficient data for boot UUID")
+	expectedSignature := uint16(0xbbb0)
+	data, signature, _ := utils.Take(data, 2)
+	if binary.LittleEndian.Uint16(signature) != expectedSignature {
+		return nil, nil, fmt.Errorf("invalid boot signature: expected 0x%x, got 0x%x", expectedSignature, signature)
 	}
-	bootUUIDBytes := data[offset : offset+16]
-	boot.BootUUID = fmt.Sprintf("%08X-%04X-%04X-%04X-%012X",
-		binary.BigEndian.Uint32(bootUUIDBytes[0:4]),
-		binary.BigEndian.Uint16(bootUUIDBytes[4:6]),
-		binary.BigEndian.Uint16(bootUUIDBytes[6:8]),
-		binary.BigEndian.Uint16(bootUUIDBytes[8:10]),
-		bootUUIDBytes[10:16])
-	offset += 16
+	boot.Signature = binary.LittleEndian.Uint16(signature)
 
-	boot.TimebaseNumerator = binary.LittleEndian.Uint32(data[offset:])
-	offset += 4
-	boot.TimebaseDenominator = binary.LittleEndian.Uint32(data[offset:])
-	offset += 4
-	boot.BootTime = int64(binary.LittleEndian.Uint64(data[offset:]))
-	offset += 8
-	boot.TimezoneOffsetMins = binary.LittleEndian.Uint32(data[offset:])
-	offset += 4
-	boot.DaylightSavings = binary.LittleEndian.Uint32(data[offset:])
-	offset += 4
+	data, headerSize, _ := utils.Take(data, 2)
+	boot.HeaderSize = binary.LittleEndian.Uint16(headerSize)
+
+	data, unknown, _ := utils.Take(data, 4)
+	boot.Unknown = binary.LittleEndian.Uint32(unknown)
+
+	data, bootUUID, _ := utils.Take(data, 16)
+	boot.BootUUID = fmt.Sprintf("%08X%04X%04X%04X%012X",
+		binary.BigEndian.Uint32(bootUUID[0:4]),
+		binary.BigEndian.Uint16(bootUUID[4:6]),
+		binary.BigEndian.Uint16(bootUUID[6:8]),
+		binary.BigEndian.Uint16(bootUUID[8:10]),
+		bootUUID[10:16])
+
+	data, timebaseNumerator, _ := utils.Take(data, 4)
+	boot.TimebaseNumerator = binary.LittleEndian.Uint32(timebaseNumerator)
+
+	data, timebaseDenominator, _ := utils.Take(data, 4)
+	boot.TimebaseDenominator = binary.LittleEndian.Uint32(timebaseDenominator)
+
+	data, bootTime, _ := utils.Take(data, 8)
+	boot.BootTime = int64(binary.LittleEndian.Uint64(bootTime))
+
+	data, timezoneOffsetMins, _ := utils.Take(data, 4)
+	boot.TimezoneOffsetMins = binary.LittleEndian.Uint32(timezoneOffsetMins)
+
+	data, daylightSavings, _ := utils.Take(data, 4)
+	boot.DaylightSavings = binary.LittleEndian.Uint32(daylightSavings)
 
 	boot.TimesyncRecords = make([]TimesyncRecord, 0)
 
-	return boot, offset, nil
+	return boot, data, nil
 }
 
-// parseTimesyncRecord parses a timesync record
-func parseTimesyncRecord(data []byte, offset int) (*TimesyncRecord, int, error) {
-	if offset+32 > len(data) { // Minimum size for timesync record
-		return nil, offset, fmt.Errorf("insufficient data for timesync record")
-	}
-
-	signature := binary.LittleEndian.Uint32(data[offset:])
-	if signature != 0x207354 {
-		return nil, offset, fmt.Errorf("invalid timesync signature: expected 0x207354, got 0x%x", signature)
-	}
-
+// ParseTimesyncRecord parses a timesync record
+func ParseTimesyncRecord(data []byte) (*TimesyncRecord, []byte, error) {
 	record := &TimesyncRecord{}
-	record.Signature = signature
-	offset += 4
+	data, signature, _ := utils.Take(data, 4)
 
-	record.UnknownFlags = binary.LittleEndian.Uint32(data[offset:])
-	offset += 4
-	record.KernelTime = binary.LittleEndian.Uint64(data[offset:])
-	offset += 8
-	record.WallTime = int64(binary.LittleEndian.Uint64(data[offset:]))
-	offset += 8
-	record.Timezone = binary.LittleEndian.Uint32(data[offset:])
-	offset += 4
-	record.DaylightSavings = binary.LittleEndian.Uint32(data[offset:])
-	offset += 4
+	if binary.LittleEndian.Uint32(signature) != expectedTimesyncSignature {
+		return record, data, fmt.Errorf("invalid timesync signature: expected 0x%x, got 0x%x", expectedTimesyncSignature, signature)
+	}
 
-	return record, offset, nil
+	data, unknownFlags, _ := utils.Take(data, 4)
+	data, kernelTime, _ := utils.Take(data, 8)
+	data, wallTime, _ := utils.Take(data, 8)
+	data, timezone, _ := utils.Take(data, 4)
+	data, daylightSavings, _ := utils.Take(data, 4)
+
+	record.Signature = binary.LittleEndian.Uint32(signature)
+	record.UnknownFlags = binary.LittleEndian.Uint32(unknownFlags)
+	record.KernelTime = binary.LittleEndian.Uint64(kernelTime)
+	record.WallTime = int64(binary.LittleEndian.Uint64(wallTime))
+	record.Timezone = binary.LittleEndian.Uint32(timezone)
+	record.DaylightSavings = binary.LittleEndian.Uint32(daylightSavings)
+
+	return record, data, nil
 }
 
 // GetTimestamp calculates a Unix epoch timestamp from mach time using timesync data
-// This implements the complex timestamp calculation logic from the Rust parser
 func GetTimestamp(
 	timesyncData map[string]*TimesyncBoot,
 	bootUUID string,
 	firehoseLogDeltaTime uint64,
 	firehosePreambleTime uint64,
 ) float64 {
-	/*  Timestamp calculation logic (from Rust implementation):
+	/*  Timestamp calculation logic:
 		Firehose Log entry timestamp is calculated by using firehose_preamble_time, firehose.continous_time_delta, and timesync timestamps
 		Firehose log header/preample contains a base timestamp
 		  Ex: Firehose header base time is 2022-01-01 00:00:00
@@ -217,85 +182,34 @@ func GetTimestamp(
 	   Add results to timesync_walltime (unix epoch in nanoseconds)
 	   Final results is unix epoch timestamp in nano seconds
 	*/
+	var timebaseAdjustment float64 = 1.0
+	var timesyncContinousTime uint64
+	var timesyncWalltime int64
 
-	var timesyncContinuousTime uint64
-	var timesyncWallTime int64
-
-	// Apple Intel uses 1/1 as the timebase
-	timebaseAdjustment := 1.0
-
-	// Try different UUID format variations to find a match
-	var foundTimesync *TimesyncBoot
-
-	// Try the lookup with different case variations and format variations
-	testUUIDs := []string{
-		bootUUID,                              // Original format
-		strings.ToUpper(bootUUID),             // Uppercase
-		strings.ToLower(bootUUID),             // Lowercase
-		strings.ReplaceAll(bootUUID, "-", ""), // Remove dashes
-		strings.ToUpper(strings.ReplaceAll(bootUUID, "-", "")), // Uppercase without dashes
-		strings.ToLower(strings.ReplaceAll(bootUUID, "-", "")), // Lowercase without dashes
-	}
-
-	for _, testUUID := range testUUIDs {
-		if ts, ok := timesyncData[testUUID]; ok {
-			foundTimesync = ts
-			break
+	if timesync, exists := timesyncData[bootUUID]; exists {
+		if timesync.TimebaseNumerator == 125 && timesync.TimebaseDenominator == 3 {
+			timebaseAdjustment = 125.0 / 3.0
 		}
-	}
-
-	// If still no match, try case-insensitive search through all keys
-	if foundTimesync == nil {
-		for uuid, ts := range timesyncData {
-			if strings.EqualFold(uuid, bootUUID) {
-				foundTimesync = ts
+		// A preamble time of 0 means we need to use the timesync header boot time as our minimum value.
+		// We also set the timesync_continous_time to zero
+		if firehosePreambleTime == 0 {
+			timesyncContinousTime = 0
+			timesyncWalltime = timesync.BootTime
+		}
+		for _, timesyncRecord := range timesync.TimesyncRecords {
+			if timesyncRecord.KernelTime > firehoseLogDeltaTime {
+				if timesyncContinousTime == 0 && timesyncWalltime == 0 {
+					timesyncContinousTime = timesyncRecord.KernelTime
+					timesyncWalltime = timesyncRecord.WallTime
+				}
 				break
 			}
+			timesyncContinousTime = timesyncRecord.KernelTime
+			timesyncWalltime = timesyncRecord.WallTime
 		}
 	}
-
-	if foundTimesync == nil {
-		// If no match found, return the raw mach time (which produces 1969/1970 timestamps)
-		// This is better than a hardcoded fallback as it preserves relative timing
-		return float64(firehoseLogDeltaTime)
-	}
-
-	timesync := foundTimesync
-
-	if timesync.TimebaseNumerator == 125 && timesync.TimebaseDenominator == 3 {
-		// For Apple Silicon (ARM) we need to adjust the mach time by multiplying by 125.0/3.0 to get the accurate nanosecond count
-		timebaseAdjustment = 125.0 / 3.0
-	}
-
-	// A preamble time of 0 means we need to find the appropriate timesync correlation point
-	// for the firehoseLogDeltaTime (which is a mach absolute time)
-	if firehosePreambleTime == 0 {
-		// Find the timesync record that correlates with this mach time
-		// We'll search through timesync records to find the right correlation point
-		timesyncContinuousTime = 0
-		timesyncWallTime = 0
-	}
-
-	for _, timesyncRecord := range timesync.TimesyncRecords {
-		if timesyncRecord.KernelTime > firehoseLogDeltaTime {
-			if timesyncContinuousTime == 0 && timesyncWallTime == 0 {
-				timesyncContinuousTime = timesyncRecord.KernelTime
-				timesyncWallTime = timesyncRecord.WallTime
-			}
-			break
-		}
-
-		timesyncContinuousTime = timesyncRecord.KernelTime
-		timesyncWallTime = timesyncRecord.WallTime
-	}
-
-	// Calculate the continuous time difference and apply timebase adjustment
-	firehoseAdjusted := float64(firehoseLogDeltaTime) * timebaseAdjustment
-	timesyncAdjusted := float64(timesyncContinuousTime) * timebaseAdjustment
-	continuousTime := firehoseAdjusted - timesyncAdjusted
-	result := continuousTime + float64(timesyncWallTime)
-
-	return result
+	continousTime := float64(firehoseLogDeltaTime)*timebaseAdjustment - float64(timesyncContinousTime)*timebaseAdjustment
+	return continousTime + float64(timesyncWalltime)
 }
 
 // NormalizeBootUUID normalizes boot UUID format to match timesync data format
