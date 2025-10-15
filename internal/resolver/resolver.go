44f6158698b5aff9c862e8c0c636a3c37ae51991
Joe Sirianni <joe.sirianni@observiq.com>
2025-10-15 15:39:44 -0400

    LLM feedback, resolve ci failures (add license)

--- internal/resolver/resolver.go
+++ internal/resolver/resolver.go
@@ -1,4 +1,18 @@
-// Resolver provides utilities for configuring the global DNS
+// Copyright observIQ, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package resolver provides utilities for configuring the global DNS
 // resolver.
 package resolver
 
@@ -128,7 +142,7 @@ func (r *Resolver) Configure() error {
 		PreferGo:     true,
 		StrictErrors: currentResolver.StrictErrors,
 
-		// "address" is the origonal DNS server address that is overloaded
+		// "address" is the original DNS server address that is overloaded
 		// by the user configured DNS server.
 		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
 			d := net.Dialer{
