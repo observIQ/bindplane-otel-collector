89ae18cdf242c96d19ac4639892431e7ab4ed532
Andy Keller <andykellr@users.noreply.github.com>
2025-10-16 21:41:05 -0400

    sync.Map and better pooling

--- extension/opampgateway/connection.go
+++ extension/opampgateway/connection.go
@@ -1,17 +1,33 @@
 package opampgateway
 
 import (
+	"sync/atomic"
+
 	"github.com/gorilla/websocket"
 	"go.uber.org/zap"
 )
 
 type connection struct {
-	id   string
-	conn *wsConnection
+	id    string
+	conn  *wsConnection
+	count atomic.Int32
 }
 
 func newConnection(conn *websocket.Conn, logger *zap.Logger) *connection {
 	return &connection{
-		conn: newWSConnection(conn, logger),
+		conn:  newWSConnection(conn, logger),
+		count: atomic.Int32{},
 	}
+}
+
+func (c *connection) agentCount() int {
+	return int(c.count.Load())
+}
+
+func (c *connection) incrementAgentCount() {
+	c.count.Add(1)
+}
+
+func (c *connection) decrementAgentCount() {
+	c.count.Add(-1)
 }
