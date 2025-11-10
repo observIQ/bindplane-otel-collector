9106a6547e202b31b79ffea891535d53d09bfbe9
Andy Keller <andykellr@users.noreply.github.com>
2025-11-10 15:32:41 -0500

    rename metrics and add latency

---
+++ extension/opampgateway/message.go
@@ -0,0 +1,21 @@
+package opampgateway
+
+import "time"
+
+type message struct {
+	data     []byte
+	received time.Time
+	number   int
+}
+
+// newMessage creates a new message from the data received
+func newMessage(number int, data []byte) *message {
+	return &message{
+		data:     data,
+		received: time.Now(),
+	}
+}
+
+func (m *message) elapsedTime() time.Duration {
+	return time.Since(m.received)
+}
