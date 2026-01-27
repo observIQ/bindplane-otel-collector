0240f459e400028e1a3d68736bb7fbc49eecaa00
Ian Adams <ian.adams@observiq.com>
2026-01-27 13:01:40 -0500

    chore(scripts): Update Available Components generation script for new honeycomb exporter

--- scripts/retrieve-available-components.sh
+++ scripts/retrieve-available-components.sh
@@ -119,6 +119,15 @@ cat go.mod | grep -E '	(go.opentelemetry.io/collector|(github.com/(open-telemetr
   } END { printf "\n"
 }' >>"$OUTPUT_DIR/$VERSION.yaml"
 
+# add entry for github.com/honeycombio/enhance-indexing-s3-exporter/enhanceindexings3exporter with version
+enhance_indexing_s3_exporter_version=$(grep -m1 'github.com/honeycombio/enhance-indexing-s3-exporter/enhanceindexings3exporter' go.mod | awk '{print $2}')
+if [ -n "$enhance_indexing_s3_exporter_version" ]; then
+  echo "        enhance_indexing_s3_exporter:" >>"$OUTPUT_DIR/$VERSION.yaml"
+  echo "          metadata:" >>"$OUTPUT_DIR/$VERSION.yaml"
+  echo "          - key: code.namespace" >>"$OUTPUT_DIR/$VERSION.yaml"
+  echo "            value: github.com/honeycombio/enhance-indexing-s3-exporter/enhanceindexings3exporter ${enhance_indexing_s3_exporter_version}" >>"$OUTPUT_DIR/$VERSION.yaml"
+fi
+
 cat go.mod | grep -E '	(go.opentelemetry.io/collector|(github.com/(open-telemetry/opentelemetry-collector-contrib|observiq/bindplane-otel-collector|observiq/observiq-otel-collector|observiq/bindplane-agent)))/extension/' | grep -v "// indirect" | grep -v "go.opentelemetry.io/collector/extension/extensiontest" | sed -E 's/^[[:space:]]*//;s/[[:space:]]*$//' | awk -F'/' 'BEGIN {
   printf "    extensions:\n      sub_component_details:\n"
   myMap["filestorage"] = "file_storage"
