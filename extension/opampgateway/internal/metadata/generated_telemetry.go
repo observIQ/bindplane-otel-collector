8487e20181fd72d82c8325a0e1a815445e5ddf33
Andy Keller <andykellr@users.noreply.github.com>
2026-02-10 09:53:17 -0500

    generate

--- extension/opampgateway/internal/metadata/generated_telemetry.go
+++ extension/opampgateway/internal/metadata/generated_telemetry.go
@@ -48,7 +48,7 @@ func (builder *TelemetryBuilder) Shutdown() {
 	builder.mu.Lock()
 	defer builder.mu.Unlock()
 	for _, reg := range builder.registrations {
-		_ = reg.Unregister() // #nosec G104 -- errors on shutdown telemetry unregister are non-critical
+		reg.Unregister()
 	}
 }
 
