385d715dd3c736e43196bf846c5226d6fe3b14d9
Ekansh <ekansh.gupta@bindplane.com>
2026-02-28 02:32:01 +0530

    chore: addressed comments for docs

--- receiver/gcspubsubeventreceiver/factory.go
+++ receiver/gcspubsubeventreceiver/factory.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate mdatagen metadata.yaml
-
 package gcspubsubeventreceiver // import "github.com/observiq/bindplane-otel-collector/receiver/gcspubsubeventreceiver"
 
 import (
