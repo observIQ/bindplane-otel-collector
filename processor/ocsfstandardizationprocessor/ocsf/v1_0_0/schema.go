9f188f73e2afc9b451276b2dad917e025db196f5
Dylan Strohschein <dylan.strohschein@bindplane.com>
2026-03-02 15:35:56 -0500

    regen

--- processor/ocsfstandardizationprocessor/ocsf/v1_0_0/schema.go
+++ processor/ocsfstandardizationprocessor/ocsf/v1_0_0/schema.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/mitchellh/mapstructure"
+	"github.com/go-viper/mapstructure/v2"
 )
 
 // AccessAnalysisResult represents the OCSF Access Analysis Result object.
