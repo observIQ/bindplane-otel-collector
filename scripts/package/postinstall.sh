531274a8d393caa3eadea4aaba47188464c72de8
Dylan Myers <dylan.myers@observiq.com>
2026-03-06 08:33:03 -0500

    feat: PIPE-875 support overridable storage and log directories

--- scripts/package/postinstall.sh
+++ scripts/package/postinstall.sh
@@ -32,6 +32,10 @@ set -e
 : "${BDOT_USER:=bdot}"
 : "${BDOT_GROUP:=bdot}"
 
+# Configurable storage and log directories
+: "${BDOT_STORAGE:=${BDOT_CONFIG_HOME}/storage}"
+: "${BDOT_LOGS:=${BDOT_CONFIG_HOME}/log}"
+
 
 install() {
     mkdir -p "${BDOT_CONFIG_HOME}"
@@ -94,7 +98,8 @@ User=root
 Group=${BDOT_GROUP}
 Environment=PATH=/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
 Environment=OIQ_OTEL_COLLECTOR_HOME=${BDOT_CONFIG_HOME}
-Environment=OIQ_OTEL_COLLECTOR_STORAGE=${BDOT_CONFIG_HOME}/storage
+Environment=OIQ_OTEL_COLLECTOR_STORAGE=${BDOT_STORAGE}
+Environment=OIQ_OTEL_COLLECTOR_LOGS=${BDOT_LOGS}
 WorkingDirectory=${BDOT_CONFIG_HOME}
 ExecStart=${BDOT_CONFIG_HOME}/observiq-otel-collector --config config.yaml
 LimitNOFILE=65000
@@ -214,7 +219,8 @@ PIDFILE=/var/run/"\$BINARY".pid
 
 # Exported variables are used by the collector process.
 export OIQ_OTEL_COLLECTOR_HOME=${BDOT_CONFIG_HOME}
-export OIQ_OTEL_COLLECTOR_STORAGE=${BDOT_CONFIG_HOME}/storage
+export OIQ_OTEL_COLLECTOR_STORAGE=${BDOT_STORAGE}
+export OIQ_OTEL_COLLECTOR_LOGS=${BDOT_LOGS}
 
 RETVAL=0
 start() {
