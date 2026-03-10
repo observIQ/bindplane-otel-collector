b600fd6e9c3e7c823170a75112201178eafd8268
Ian Adams <ian.adams@observiq.com>
2026-03-10 09:58:58 -0400

    fix(receiver/windowseventtrace): Consolidate getEventInformation calls, bulletproof STRING_ONLY events

--- receiver/windowseventtracereceiver/internal/etw/parser.go
+++ receiver/windowseventtracereceiver/internal/etw/parser.go
@@ -45,17 +45,19 @@ type parser struct {
 	logger      *zap.Logger
 }
 
-func GetEventProperties(r *advapi32.EventRecord, logger *zap.Logger) (map[string]any, error) {
-	if r.EventHeader.Flags == advapi32.EVENT_HEADER_FLAG_STRING_ONLY {
+func GetEventProperties(r *advapi32.EventRecord, logger *zap.Logger) (map[string]any, *tdh.TraceEventInfo, error) {
+	if r.EventHeader.Flags&advapi32.EVENT_HEADER_FLAG_STRING_ONLY != 0 {
 		userDataPtr := (*uint16)(unsafe.Pointer(r.UserData))
 		return map[string]any{
-			"UserData": utf16StringAtOffset(uintptr(unsafe.Pointer(userDataPtr)), 0),
-		}, nil
+				"UserData": utf16StringAtOffset(uintptr(unsafe.Pointer(userDataPtr)), 0),
+			},
+			nil, // getEventInformation fails for string-only events, so we return nil for the TraceEventInfo
+			nil
 	}
 
 	ti, err := getEventInformation(r)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get event information: %w", err)
+		return nil, nil, fmt.Errorf("failed to get event information: %w", err)
 	}
 
 	p := &parser{
@@ -72,13 +74,13 @@ func GetEventProperties(r *advapi32.EventRecord, logger *zap.Logger) (map[string
 		propName := windowsSys.UTF16PtrToString((*uint16)(namePtr))
 		value, err := p.getPropertyValue(r, ti, uint32(i))
 		if err != nil {
-			return nil, fmt.Errorf("failed to get property value: %w", err)
+			return nil, nil, fmt.Errorf("failed to get property value: %w", err)
 		}
 
 		properties[propName] = value
 	}
 
-	return properties, nil
+	return properties, ti, nil
 }
 
 func (p *parser) getPropertyValue(r *advapi32.EventRecord, propInfo *tdh.TraceEventInfo, i uint32) (any, error) {
