5278d6048c8a0fc916aa0817c25cd8cb23016bde
Dakota Paasman <122491662+dpaasman00@users.noreply.github.com>
2026-03-11 15:33:53 -0400

    Update scripts/retrieve-available-components.sh
    Co-authored-by: Ian Adams <ian.sam.adams@gmail.com>

--- scripts/retrieve-available-components.sh
+++ scripts/retrieve-available-components.sh
@@ -55,7 +55,7 @@ cat go.mod | grep -E '	(go.opentelemetry.io/collector|(github.com/(open-telemetr
     myMap["awss3event"] = "s3event"
     myMap["mongodbatlas"] = "mongodb_atlas"
     myMap["azureeventhub"] = "azure_event_hub"
-    myMap["gcspubsubeventreceiver"] = "gcsevent" 
+    myMap["gcspubsubevent"] = "gcsevent" 
 } {
   split($NF, parts, " ")
   name=parts[1]
