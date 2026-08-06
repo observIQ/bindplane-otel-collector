eb4b3199c96d39460aa863cb34119f7aeb37e5be
Nico Stewart <nico.stewart@dynatrace.com>
2026-08-06 12:27:19 -0400

    Add comments to wrapper methods

--- internal/extension/opampconnectionextension/internal/opamp/observiq/gated_opamp_client.go
+++ internal/extension/opampconnectionextension/internal/opamp/observiq/gated_opamp_client.go
@@ -36,56 +36,78 @@ func newGatedOpAMPClient(underlying client.OpAMPClient, gate *sendGate) client.O
 	return &gatedOpAMPClient{OpAMPClient: underlying, gate: gate}
 }
 
+// SetAgentDescription calls [client.OpAMPClient.SetAgentDescription] with its OpAMPClient after making sure
+// the sendGate isn't blocking
 func (g *gatedOpAMPClient) SetAgentDescription(descr *protobufs.AgentDescription) error {
 	g.gate.wait()
 	return g.OpAMPClient.SetAgentDescription(descr)
 }
 
+// SetHealth calls [client.OpAMPClient.SetHealth] with its OpAMPClient after making sure
+// the sendGate isn't blocking
 func (g *gatedOpAMPClient) SetHealth(health *protobufs.ComponentHealth) error {
 	g.gate.wait()
 	return g.OpAMPClient.SetHealth(health)
 }
 
+// UpdateEffectiveConfig calls [client.OpAMPClient.UpdateEffectiveConfig] with its OpAMPClient after making sure
+// the sendGate isn't blocking
 func (g *gatedOpAMPClient) UpdateEffectiveConfig(ctx context.Context) error {
 	g.gate.wait()
 	return g.OpAMPClient.UpdateEffectiveConfig(ctx)
 }
 
+// SetRemoteConfigStatus calls [client.OpAMPClient.SetRemoteConfigStatus] with its OpAMPClient after making sure
+// the sendGate isn't blocking
 func (g *gatedOpAMPClient) SetRemoteConfigStatus(status *protobufs.RemoteConfigStatus) error {
 	g.gate.wait()
 	return g.OpAMPClient.SetRemoteConfigStatus(status)
 }
 
+// SetPackageStatuses calls [client.OpAMPClient.SetPackageStatuses] with its OpAMPClient after making sure
+// the sendGate isn't blocking
 func (g *gatedOpAMPClient) SetPackageStatuses(statuses *protobufs.PackageStatuses) error {
 	g.gate.wait()
 	return g.OpAMPClient.SetPackageStatuses(statuses)
 }
 
+// RequestConnectionSettings calls [client.OpAMPClient.RequestConnectionSettings] with its OpAMPClient after making sure
+// the sendGate isn't blocking
 func (g *gatedOpAMPClient) RequestConnectionSettings(request *protobufs.ConnectionSettingsRequest) error {
 	g.gate.wait()
 	return g.OpAMPClient.RequestConnectionSettings(request)
 }
 
+// SetCustomCapabilities calls [client.OpAMPClient.SetCustomCapabilities] with its OpAMPClient after making sure
+// the sendGate isn't blocking
 func (g *gatedOpAMPClient) SetCustomCapabilities(customCapabilities *protobufs.CustomCapabilities) error {
 	g.gate.wait()
 	return g.OpAMPClient.SetCustomCapabilities(customCapabilities)
 }
 
+// SetFlags calls [client.OpAMPClient.SetFlags] with its OpAMPClient after making sure
+// the sendGate isn't blocking
 func (g *gatedOpAMPClient) SetFlags(flags protobufs.AgentToServerFlags) {
 	g.gate.wait()
 	g.OpAMPClient.SetFlags(flags)
 }
 
+// SendCustomMessage calls [client.OpAMPClient.SendCustomMessage] with its OpAMPClient after making sure
+// the sendGate isn't blocking
 func (g *gatedOpAMPClient) SendCustomMessage(message *protobufs.CustomMessage) (chan struct{}, error) {
 	g.gate.wait()
 	return g.OpAMPClient.SendCustomMessage(message)
 }
 
+// SetAvailableComponents calls [client.OpAMPClient.SetAvailableComponents] with its OpAMPClient after making sure
+// the sendGate isn't blocking
 func (g *gatedOpAMPClient) SetAvailableComponents(components *protobufs.AvailableComponents) error {
 	g.gate.wait()
 	return g.OpAMPClient.SetAvailableComponents(components)
 }
 
+// SetCapabilities calls [client.OpAMPClient.SetCapabilities] with its OpAMPClient after making sure
+// the sendGate isn't blocking
 func (g *gatedOpAMPClient) SetCapabilities(capabilities *protobufs.AgentCapabilities) error {
 	g.gate.wait()
 	return g.OpAMPClient.SetCapabilities(capabilities)
