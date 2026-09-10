42431756b005c941f0e59498d62e68a639373fa9
Cole Laven <82364622+colelaven@users.noreply.github.com>
2026-09-10 13:56:49 -0400

    fix: register Windows event source for collector logs (#3643)

--- windows/test/install.rb
+++ windows/test/install.rb
@@ -28,3 +28,9 @@ describe service('observiq-otel-collector') do
     it { should be_enabled }
     it { should be_running }
 end
+
+describe registry_key('HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EventLog\Application\observiq-otel-collector') do
+    it { should exist }
+    its('EventMessageFile') { should match(/EventCreate\.exe$/) }
+    its('TypesSupported') { should cmp 7 }
+end
